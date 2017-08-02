80f1ea8bdf65610bf105618c51acb551486b04b4
so yeon <wjddbselr@naver.com>
2017-08-02 11:04:45 +0900

    2017-08-02

--- app/src/main/java/app/gotogether/com/mapactivity/MapsActivity.java
+++ app/src/main/java/app/gotogether/com/mapactivity/MapsActivity.java
@@ -14,7 +14,6 @@ import android.os.Build;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
